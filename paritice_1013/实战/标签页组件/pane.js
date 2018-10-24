c6082d9020fe0c62e4c1754a1c81e3e4a49daf90
xioaixoa <34847886+xioaixoa@users.noreply.github.com>
2018-10-24 17:21:37 +0800

    10.24

---
+++ paritice_1013/实战/标签页组件/pane.js
@@ -0,0 +1,34 @@
+Vue.component('pane',{
+    name:'pane',
+    template:'\
+        <div class="pane" v-show="show">\
+            <slot>父组件没东西默认显示我</slot>\
+        </div>',
+    props:{
+        name:{
+            type:String
+        },
+        label:{
+            type:String,
+            default:''
+         },
+    },
+    data:function(){
+        return {
+            show:true
+        }
+    },
+    methods:{
+        updateNav:function(){
+            this.$parent.updateNav();
+        }
+    },
+    watch:{
+        label:function(){
+            this.updateNav();
+        }
+    },
+    mounted:function(){
+        this.updateNav();
+    }
+});
